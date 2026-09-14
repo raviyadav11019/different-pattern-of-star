7859bb596a25b072d90d4ca2fca31b5a8341ab49
raviyadav11019 <raviyadav11019@gmail.com>
2026-09-14 09:49:18 +0000

    pattern 1 code done

---
+++ pattern.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main(){
+    int i,n,j;
+   
+    for(i=1;i<=5;i++){
+        printf("\n");
+        for(j=0;j<i;j++){
+            printf("*");
+        }
+    }
+return 0;
+}
