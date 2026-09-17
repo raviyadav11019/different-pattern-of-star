eb31c752b355d5a3747020bb208b9fbf09bad9e4
Ravi Kumar Yadav <raviyadav11019@gmail.com>
2026-09-17 17:59:19 +0000

    add new tw0 pattern

--- pattern.c
+++ pattern.c
@@ -1,3 +1,5 @@
+//this is first normal start pattern
+
 #include<stdio.h>
 int main(){
     int i,n,j;
@@ -8,5 +10,30 @@ int main(){
             printf("*");
         }
     }
+
+//this is second square box pattern 
+
+int m,o;
+for(m=1;m<=5;m++){
+    printf("\n");
+    for(o=1;o<=5;o++){
+        printf("%d",o);
+    }
+}
+printf("\n");
+
+//this is third number pattern like first star type question
+
+int p,q;
+for(p=1;p<=5;p++){
+    
+    for(q=1;q<=p;q++)
+        printf("%d",q);
+        printf("\n");
+    }
 return 0;
 }
+
+//this is fourth tiangle star pattern 
+
+int x,y;
